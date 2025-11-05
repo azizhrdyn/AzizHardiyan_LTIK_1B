7c872be4805177fddb62f59f4a6d646b2d752cd4
azizhrdyn <azizhardiyan@gmail.com>
2025-11-05 22:26:46 +0700

    add login

--- Initial_LTIK.py
+++ Initial_LTIK.py
@@ -1,1 +1,33 @@
-#Initial repository
+def intro():
+    print("=== APLIKASI ADMINISTRASI SEKOLAH ===")
+    print("Username: SMAN 1 Karawang")
+    print()
+
+def login(pwsistem):
+    uinput = 2 
+
+    while uinput >= 0:
+        
+        if uinput == 2:
+            pw = input("Password (3 kali kesempatan): ")
+        else:
+            pw = input("Password: ")
+
+        if uinput == 0:
+            print("Akses ditolak, anda telah gagal mencoba sebanyak 3 kali.")
+            return False  
+        
+        elif pw == pwsistem:
+            print("Akses diterima. Selamat datang di aplikasi.")
+            return True  
+        
+        else:
+            print(f"Password salah. Sisa kesempatan: {uinput}")
+            uinput -= 1
+
+def main():
+    pwsistem = "Latihan"
+    intro()
+    login(pwsistem)
+
+main()
